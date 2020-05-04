87736f4c390ac6f006298aca02ad4d4ad174b7aa
ralanlittle <alanl@us.ibm.com>
2020-05-04 16:00:17 +0000

    8

--- src/main/java/dev/appsody/starter/StarterApplication.java
+++ src/main/java/dev/appsody/starter/StarterApplication.java
@@ -2,7 +2,6 @@ package dev.appsody.starter;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import system.out.*;
 
 @ApplicationPath("/starter")
 public class StarterApplication extends Application {
